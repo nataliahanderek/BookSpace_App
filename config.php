892bd45d7fb929936fdb560b660fa9735c3ee6ad
nataliahanderek <72739947+nataliahanderek@users.noreply.github.com>
2022-01-07 17:48:58 +0100

    Hide config file

--- config.php
+++ config.php
@@ -1,10 +1,5 @@
 <?php
 
-/*const USERNAME = 'eisgcesknkyrja';
-const PASSWORD = 'dcdc55c8bbc6a7223f11a4f4d55ae243e439754371a094d2119c318ffde35d7c';
-const HOST = 'ec2-52-19-164-214.eu-west-1.compute.amazonaws.com';
-const DATABASE = 'dvd3dffcgianv';*/
-
 const USERNAME = 'ldonsqtbuvlioz';
 const PASSWORD = '7cc0e644c45221912498a521f5301c8a79cd10bce9f351e44962380dfc31d3df';
 const HOST = 'ec2-34-246-24-110.eu-west-1.compute.amazonaws.com';
