946c2a7e8dc6ebc8c0cdabb7fbb2892625b551f4
nataliahanderek <72739947+nataliahanderek@users.noreply.github.com>
2022-01-12 10:07:04 +0100

    Add config to gitignore

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-const USERNAME = 'ldonsqtbuvlioz';
-const PASSWORD = '7cc0e644c45221912498a521f5301c8a79cd10bce9f351e44962380dfc31d3df';
-const HOST = 'ec2-34-246-24-110.eu-west-1.compute.amazonaws.com';
-const DATABASE = 'da591oadcb36d4';
